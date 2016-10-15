eee26fade7f0b06ade49bfc1e96c1835af2e8fde
Metin Yılmaz <metinyilmazmail@gmail.com>
2016-10-15 22:09:52 +0300

    js test

--- js/menu.js
+++ js/menu.js
@@ -1,65 +1,65 @@
 /*document.addEventListener('DOMContentLoaded', function() {
 
-  var buttons = document.querySelectorAll('.menu-button');
-  var active = ('menuitem-is-open');
+ var buttons = document.querySelectorAll('.menu-button');
+ var active = ('menuitem-is-open');
 
-  function closeMenus() {
-    for (var i = 0; i < buttons.length; i++) {
+ function closeMenus() {
+ for (var i = 0; i < buttons.length; i++) {
 
-      var button = buttons[i];
-      var menuId = button.getAttribute('aria-controls');
-      var menu = document.getElementById(menuId);
-      var state = menu.getAttribute('aria-hidden');
+ var button = buttons[i];
+ var menuId = button.getAttribute('aria-controls');
+ var menu = document.getElementById(menuId);
+ var state = menu.getAttribute('aria-hidden');
 
-      if (state == 'false') {
-        menu.setAttribute('aria-hidden', 'true');
-        button.parentNode.classList.remove(active);
-      }
-    }
-  }
+ if (state == 'false') {
+ menu.setAttribute('aria-hidden', 'true');
+ button.parentNode.classList.remove(active);
+ }
+ }
+ }
 
-  function openMenu(event) {
+ function openMenu(event) {
 
-    var button = event.target;
-    var menuId = button.getAttribute('aria-controls');
-    var menu = document.getElementById(menuId);
-    var state = menu.getAttribute('aria-hidden');
+ var button = event.target;
+ var menuId = button.getAttribute('aria-controls');
+ var menu = document.getElementById(menuId);
+ var state = menu.getAttribute('aria-hidden');
 
-    if (state == 'false') {
-      menu.setAttribute('aria-hidden', 'true');
-      button.parentNode.classList.remove(active);
-    } else {
-      closeMenus();
+ if (state == 'false') {
+ menu.setAttribute('aria-hidden', 'true');
+ button.parentNode.classList.remove(active);
+ } else {
+ closeMenus();
 
-      menu.setAttribute('aria-hidden', 'false');
-      button.parentNode.classList.add(active);
-    }
-  }
+ menu.setAttribute('aria-hidden', 'false');
+ button.parentNode.classList.add(active);
+ }
+ }
 
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', openMenu);
-  }
-});
-*/
-(function($) {
-  $(function () {
-    $('.menu-button').on('mouseenter', function (event) {
-      var $t = $(this);
-      var menuId = $t.attr('aria-controls');
-      var menu = $('#'+menuId);
+ for (var i = 0; i < buttons.length; i++) {
+ buttons[i].addEventListener('click', openMenu);
+ }
+ });
+ */
+(function ($) {
+    $(function () {
+        $('.menu-button').parent()
+            .on('mouseenter', function (event) {
+                var $t = $(this);
+                var menuId = $t.attr('aria-controls');
+                var menu = $('#' + menuId);
 
-      menu.attr('aria-hidden', 'false');
+                menu.attr('aria-hidden', 'false');
 
+            })
+            .on('mouseleave', function (event) {
+                var $t = $(this);
+                var menuId = $t.attr('aria-controls');
+                var menu = $('#' + menuId);
+
+                menu.attr('aria-hidden', 'true');
+            });
     });
-
-    $('.menu-button').on('mouseleave', function (event) {
-      var $t = $(this);
-      var menuId = $t.attr('aria-controls');
-      var menu = $('#'+menuId);
-
-      menu.attr('aria-hidden', 'true');
-    });
-  });
 
 })(jQuery);
 
