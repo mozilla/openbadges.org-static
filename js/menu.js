1b79dd053f6fb21cb3bc48b8ff7ee18e5b14c9c2
Metin Yılmaz <metinyilmazmail@gmail.com>
2016-10-15 22:36:14 +0300

    js test

--- js/menu.js
+++ js/menu.js
@@ -42,30 +42,48 @@
  });
  */
 (function ($) {
+    window.menuOpen= function($t){
+        if (window.abc) {
+            window.clearTimeout(window.abc);
+            window.abc = false;
+        }
+
+        var menuId = $t.find('.menu-button').attr('aria-controls');
+        var menu = $('#' + menuId);
+
+        menu.attr('aria-hidden', 'false');
+        $t.addClass('menuitem-is-open');
+    };
+
+    window.menuClose = function(){
+        var $t = $('.menuitem-is-open');
+        var menuId = $t.find('.menu-button').attr('aria-controls');
+        var menu = $('#' + menuId);
+
+        menu.attr('aria-hidden', 'true');
+        $t.removeClass('menuitem-is-open');
+        window.abc = false;
+    };
+
     $(function () {
         $('.menu-button').parent()
             .on('mouseenter', function (event) {
-                if (window.abc) {
-                    window.clearTimeout(window.abc);
-                    window.abc = false;
-                }
+                window.menuOpen($(this));
+            })
+            .on('mouseleave', function (event) {
+                window.abc = window.setTimeout(window.menuClose, 1000);
+            })
+            .on('click', function(){
                 var $t = $(this);
                 var menuId = $t.find('.menu-button').attr('aria-controls');
                 var menu = $('#' + menuId);
 
-                menu.attr('aria-hidden', 'false');
-                $t.addClass('menuitem-is-open');
-            })
-            .on('mouseleave', function (event) {
-                window.abc = window.setTimeout(function () {
-                    var $t = $('.menuitem-is-open');
-                    var menuId = $t.find('.menu-button').attr('aria-controls');
-                    var menu = $('#' + menuId);
-
-                    menu.attr('aria-hidden', 'true');
-                    $t.removeClass('menuitem-is-open');
-                    window.abc = false;
-                }, 1000);
+               if( menu.attr('aria-hidden') == true){
+                   window.menuOpen($t);
+               }
+                else{
+                   window.menuClose();
+               }
             });
     });
 
